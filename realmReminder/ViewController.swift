b3650c14bcf985d1f6f2de5996ee8ccb7c8197e4
Maxim Mitin <maximus405@gmail.com>
2021-11-27 20:54:58 +0300

    Initial Commit

---
+++ realmReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realmReminder
+//
+//  Created by Maxim Mitin on 27.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
